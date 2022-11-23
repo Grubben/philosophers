bde3abe37672581e76b9e1d4a9f8237d62668d4b
endarc <arc@end.com>
2022-11-23 22:39:28 +0000

    Starting to understand how to make threads

--- philo/src/main.c
+++ philo/src/main.c
@@ -3,17 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: endarc <endarc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 18:17:36 by amaria-d          #+#    #+#             */
-/*   Updated: 2022/11/18 18:17:52 by amaria-d         ###   ########.fr       */
+/*   Updated: 2022/11/23 22:35:06 by endarc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philosophers.h"
+#include "philo.h"
+
+void	*hello(void *arg1)
+{
+	printf("I'm a created thread!\n");
+	return (arg1);
+}
 
 int	main(void)
 {
+	pthread_t		th;
+	// pthread_attr_t	thattr;
+
+	printf("%d\n", pthread_create(&th, NULL, hello, NULL));
+	printf("%d\n", pthread_create(&th, NULL, hello, NULL));
+	printf("%d\n", pthread_create(&th, NULL, hello, NULL));
+	printf("%d\n", pthread_create(&th, NULL, hello, NULL));
+	printf("%d\n", pthread_create(&th, NULL, hello, NULL));
+	usleep(1000000);
 
 	return (0);
 }
