d29f8101be2dcbeb2917921d8abccada42311ebb
endarc <arc@end.com>
2022-11-24 12:41:56 +0000

    Creating philosophers correctly!

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,48 +6,72 @@
 /*   By: endarc <endarc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 18:17:36 by amaria-d          #+#    #+#             */
-/*   Updated: 2022/11/24 10:15:23 by endarc           ###   ########.fr       */
+/*   Updated: 2022/11/24 12:37:50 by endarc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*hello(void *arg1)
+void	*philo_go(void *arg)
 {
-	printf("I'm a created thread!\n");
-	usleep(1000000);
-	return (arg1);
+	t_philo	philo;
+
+	philo = *(t_philo *)arg;
+	printf("Philosopher %ld Active\n", philo.id);
+	usleep(6000000);
+	return (NULL);
+}
+
+int	philos_create(t_geninfo *worldata)
+{
+	size_t	i;
+	t_philo	*tmphilo;
+
+	// Needs to be freed!!!
+	worldata->philarr = ft_calloc((worldata->n_philos), sizeof(t_philo));
+	i = 0;
+	while (i < worldata->n_philos)
+	{
+		tmphilo = &worldata->philarr[i];
+		tmphilo->id = i + 1;
+		pthread_create(&(tmphilo->thread), NULL, philo_go, tmphilo);
+		usleep(1000000);
+		i++;
+	}
+	return (1);
 }
 
 int	main(int argc, char *argv[])
 {
-	pthread_t		th;
-	// pthread_attr_t	thattr;
 	t_geninfo	worldattr;
 
-	if (argc < 5 || argc > 6)
-		return (0);
-	
+	// if (argc < 5 || argc > 6)
+	// 	return (0);
+	if (argc < 2)
+		return (printf("Not enough arguments\n") && 0);
 	worldattr.n_philos = ft_atoi(argv[1]);
-	worldattr.time_to_die = ft_atoi(argv[2]);
-	worldattr.time_to_eat = ft_atoi(argv[3]);
-	worldattr.time_to_sleep = ft_atoi(argv[4]);
-	if (argc == 6)
-		worldattr.n_must_eat = ft_atoi(argv[5]);
-	else
-		worldattr.n_must_eat = 0;
+	// worldattr.n_forks = worldattr.n_philos / 2;
 	
-	worldattr.n_forks = worldattr.n_philos / 2;
+	// worldattr.time_to_die = ft_atoi(argv[2]);
+	// worldattr.time_to_eat = ft_atoi(argv[3]);
+	// worldattr.time_to_sleep = ft_atoi(argv[4]);
+	// if (argc == 6)
+	// 	worldattr.n_must_eat = ft_atoi(argv[5]);
+	// else
+	// 	worldattr.n_must_eat = 0;
+
+	philos_create(&worldattr);
+	// usleep(3000000);
+	// worldattr.philarr = ft_calloc((worldattr.n_philos), sizeof(t_philo));
+	// tmphilo = worldattr.philarr[0];
+	// tmphilo.id = 0 + 1;
+	// pthread_create(&(worldattr.philarr[0].thread), NULL, philo_go, &tmphilo);
 
 
+	// This is bad. Just using while learning
+	pthread_join(worldattr.philarr[worldattr.n_philos - 1].thread, NULL);
+	// pthread_join(worldattr.philarr[0].thread, NULL);
 
-
-	printf("%d\n", pthread_create(&th, NULL, hello, NULL));
-	printf("%d\n", pthread_create(&th, NULL, hello, NULL));
-	// printf("%d\n", pthread_create(&th, NULL, hello, NULL));
-	// printf("%d\n", pthread_create(&th, NULL, hello, NULL));
-	// printf("%d\n", pthread_create(&th, NULL, hello, NULL));
-	usleep(1000000);
-
+	// usleep(3000000);
 	return (0);
 }
--- philo/src/philo.h
+++ philo/src/philo.h
@@ -3,27 +3,43 @@
 
 # include <stdio.h>
 # include <unistd.h>
+# include <stdlib.h>
+
 # include <pthread.h>
-
-typedef struct s_geninfo
-{
-    size_t  n_philos;
-    size_t  time_to_die;
-    size_t  time_to_eat;
-    size_t  time_to_sleep;
-    size_t  n_must_eat;
-
-    size_t  n_forks;
-}               t_geninfo;
 
 enum e_PHILOSTATE
 {
-    EAT,
-    SLEEP,
-    THINK
+	EAT,
+	SLEEP,
+	THINK
 };
+
+typedef struct s_philo
+{
+	pthread_t   thread;
+	size_t		id;
+
+	int			state; // e_PHILOSTATE
+}               t_philo;
+
+
+typedef struct s_geninfo
+{
+	size_t	n_philos;
+	t_philo	*philarr;
+	size_t	n_forks;
+
+	size_t  time_to_die;
+	size_t  time_to_eat;
+	size_t  time_to_sleep;
+	size_t  n_must_eat;
+
+}               t_geninfo;
+
 
 /*  UTILS   */
 int	ft_atoi(const char *str);
+
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
