9cf8698b061015831bbfd08805fd50ccb0d645fa
endarc <arc@end.com>
2022-11-24 10:41:40 +0000

    Starting to add general info struct. Might be too soon. Not used to threads yet

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: endarc <endarc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 18:17:36 by amaria-d          #+#    #+#             */
-/*   Updated: 2022/11/23 22:35:06 by endarc           ###   ########.fr       */
+/*   Updated: 2022/11/24 10:15:23 by endarc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,38 @@
 void	*hello(void *arg1)
 {
 	printf("I'm a created thread!\n");
+	usleep(1000000);
 	return (arg1);
 }
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
 	pthread_t		th;
 	// pthread_attr_t	thattr;
+	t_geninfo	worldattr;
+
+	if (argc < 5 || argc > 6)
+		return (0);
+	
+	worldattr.n_philos = ft_atoi(argv[1]);
+	worldattr.time_to_die = ft_atoi(argv[2]);
+	worldattr.time_to_eat = ft_atoi(argv[3]);
+	worldattr.time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		worldattr.n_must_eat = ft_atoi(argv[5]);
+	else
+		worldattr.n_must_eat = 0;
+	
+	worldattr.n_forks = worldattr.n_philos / 2;
+
+
+
 
 	printf("%d\n", pthread_create(&th, NULL, hello, NULL));
 	printf("%d\n", pthread_create(&th, NULL, hello, NULL));
-	printf("%d\n", pthread_create(&th, NULL, hello, NULL));
-	printf("%d\n", pthread_create(&th, NULL, hello, NULL));
-	printf("%d\n", pthread_create(&th, NULL, hello, NULL));
+	// printf("%d\n", pthread_create(&th, NULL, hello, NULL));
+	// printf("%d\n", pthread_create(&th, NULL, hello, NULL));
+	// printf("%d\n", pthread_create(&th, NULL, hello, NULL));
 	usleep(1000000);
 
 	return (0);
--- philo/src/philo.h
+++ philo/src/philo.h
@@ -5,6 +5,25 @@
 # include <unistd.h>
 # include <pthread.h>
 
+typedef struct s_geninfo
+{
+    size_t  n_philos;
+    size_t  time_to_die;
+    size_t  time_to_eat;
+    size_t  time_to_sleep;
+    size_t  n_must_eat;
+
+    size_t  n_forks;
+}               t_geninfo;
+
+enum e_PHILOSTATE
+{
+    EAT,
+    SLEEP,
+    THINK
+};
+
+/*  UTILS   */
+int	ft_atoi(const char *str);
 
 #endif
-
