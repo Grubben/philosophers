172593ee5226fb7089b720c3b01edc3eba394cb1
endarc <arc@end.com>
2022-12-09 11:16:51 +0000

    Added some comments and more improvements. But SMALL, still too slow, don't know why

--- philo/src/state.c
+++ philo/src/state.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   state.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: endarc <endarc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 16:56:25 by amaria-d          #+#    #+#             */
-/*   Updated: 2022/12/07 18:47:41 by amaria-d         ###   ########.fr       */
+/*   Updated: 2022/12/09 11:06:13 by endarc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,32 +21,34 @@
 
 void	philo_eat(t_philo *philo)
 {
-	print_state(philo, EAT);
-	philo->lastmeal = get_timestamp(philo->wdata->startstamp);
 	
+	//TODO: Careful here when there is only one philosopher
 	philo->fleft->setb = 0;
 	philo->fright->setb = 0;
 	philo_forks_unlock(philo);
 	philo->forkstaken = 2;
 	
+	philo->lastmeal = get_timestamp(philo->wdata->startstamp);
+
+	print_state(philo, EAT);
 	usleep(philo->wdata->time_to_eat);
 }
 
 void	philo_sleep(t_philo *philo)
 {
-	print_state(philo, SLEEP);
 	philo_forks_lock(philo);
 	philo->fleft->setb = 1;
 	philo->fright->setb = 1;
 	philo_forks_unlock(philo);
 
+	print_state(philo, SLEEP);
 	usleep(philo->wdata->time_to_sleep);
 	print_state(philo, THINK);
 }
 
 void	philo_die(t_philo *philo)
 {
-	pthread_mutex_lock(&philo->wdata->allmutex);
+	// pthread_mutex_lock(&philo->wdata->allmutex);
 
 	if (philo->wdata->philo_died == 1)
 	{
@@ -68,7 +70,7 @@ void	sttchng(t_philo *philo)
 		pthread_mutex_lock(&philo->wdata->allmutex);
 		if (philo->wdata->philo_died)
 		{
-			pthread_mutex_unlock(&philo->wdata->allmutex);
+			// pthread_mutex_unlock(&philo->wdata->allmutex);
 			philo_die(philo);
 			break ;
 		}
@@ -83,6 +85,9 @@ void	sttchng(t_philo *philo)
 			philo_sleep(philo);
 		}
 		else
+		{
 			philo_forks_unlock(philo);
+			// usleep(philo->wdata->time_to_eat / 2);
+		}
 	}
 }
