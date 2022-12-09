d033beb41bef436ae0936f8db02d3b03ecaf4e4f
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c1r8s1.42lisboa.com>
2022-12-09 13:38:23 +0000

    prints: added print_autostate

--- philo/src/prints.c
+++ philo/src/prints.c
@@ -6,7 +6,7 @@
 /*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 15:58:58 by amaria-d          #+#    #+#             */
-/*   Updated: 2022/12/07 18:31:20 by amaria-d         ###   ########.fr       */
+/*   Updated: 2022/12/09 13:38:09 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,3 +47,32 @@ void	print_state(t_philo *philo, int state)
 	}
 	pthread_mutex_unlock(&philo->wdata->printlock);
 }
+
+void   print_autostate(t_philo *philo, int state)
+{ 
+	pthread_mutex_lock(&philo->wdata->printlock);
+	if (philo->state == THINK)
+	{
+	printf("%ld %ld is thinking\n", get_timestamp(philo->wdata->startstamp), philo->id);
+	}
+	if (philo->state == TAKEFORK)
+	{
+	printf("%ld %ld has taken a fork\n", get_timestamp(philo->wdata->startstamp), philo->id);
+	}
+	if (philo->state == EAT)
+	{
+	printf("%ld %ld is eating\n", get_timestamp(philo->wdata->startstamp), philo->id);
+	}
+	if (philo->state == RELEASEFORK)
+	{
+	printf("%ld %ld is releasing a fork\n", get_timestamp(philo->wdata->startstamp), philo->id);
+	}
+	if (philo->state == SLEEP)
+	{
+	printf("%ld %ld is sleeping\n", get_timestamp(philo->wdata->startstamp), philo->id);
+	}
+	if (philo->state == DEAD)
+	{
+		printf("%ld %ld died\n", get_timestamp(philo->wdata->startstamp), philo->id);
+	}
+	pthread_mutex_unlock(&philo->wdata->printlock);
