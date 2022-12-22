61eaf3728ac0a85c40a20c4ca24931389d969939
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c1r8s1.42lisboa.com>
2022-12-22 15:30:26 +0000

    Added arg_parse to check if arguments given are good

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,11 +6,39 @@
 /*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 18:17:36 by amaria-d          #+#    #+#             */
-/*   Updated: 2022/12/22 14:13:24 by amaria-d         ###   ########.fr       */
+/*   Updated: 2022/12/22 15:27:35 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	arg_parse(int argc, char *argv[], t_geninfo *wdata)
+{
+	if (! str_isdigit(argv[1]))
+		return (printf("Incorrect argument given\n") && 0);
+	wdata->n_philos = ft_atoi(argv[1]);
+	if (! str_isdigit(argv[2]))
+		return (printf("Incorrect argument given\n") && 0);
+	wdata->time_to_die = ft_atoi(argv[2]);
+	if (! str_isdigit(argv[3]))
+		return (printf("Incorrect argument given\n") && 0);
+	wdata->time_to_eat = ft_atoi(argv[3]);
+	if (! str_isdigit(argv[4]))
+		return (printf("Incorrect argument given\n") && 0);
+	wdata->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+	{
+		if (! str_isdigit(argv[5]))
+			return (printf("Incorrect argument given\n") && 0);
+		wdata->n_must_eat = ft_atoi(argv[5]);
+		if (wdata->n_must_eat == 0)
+			return (0);
+	}
+	else
+		wdata->n_must_eat = 0;
+	return (wdata->n_philos && wdata->time_to_die
+		&& wdata->time_to_eat && wdata->time_to_sleep);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -19,13 +47,8 @@ int	main(int argc, char *argv[])
 	if (argc < 5 || argc > 6)
 		return (printf("Not enough arguments\n") && 0);
 	memset(&wattr, 0, sizeof(t_geninfo));
-	wattr.n_philos = ft_atoi(argv[1]);
-	wattr.time_to_die = ft_atoi(argv[2]);
-	wattr.time_to_eat = ft_atoi(argv[3]);
-	wattr.time_to_sleep = ft_atoi(argv[4]);
-	wattr.n_must_eat = 0;
-	if (argc == 6)
-		wattr.n_must_eat = ft_atoi(argv[5]);
+	if (! arg_parse(argc, argv, &wattr))
+		return (printf("Incorrect argument given\n") && 0);
 	wattr.philo_died = 0;
 	wattr.n_forks = wattr.n_philos;
 	pthread_mutex_init(&wattr.allmutex, NULL);
